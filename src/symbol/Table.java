2f7afe2eb87ba1fb16682d2dc792981349dabcbf
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-03-07 08:56:19 +0100

    Add type visitor

---
+++ src/symbol/Table.java
@@ -0,0 +1,23 @@
+package Symbol;
+
+public class Table {
+    public Table() {
+    }
+
+    public void put(Symbol key, Object value) {
+    }
+
+    public Object get(Symbol key) {
+        return new Object();
+    }
+
+    public void beginScope() {
+    }
+
+    public void endScope() {
+    }
+
+    //public java.util.Enumeration keys() {
+    //    return;
+    //}
+}
