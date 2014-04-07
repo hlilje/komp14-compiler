781c84cbc3230a88d31db68fe297eaf9f1fdd672
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-04-07 17:52:11 +0200

    Add tree, assem and rename EXP

---
+++ src/tree/EXPR.java
@@ -0,0 +1,10 @@
+package tree;
+public class EXPR extends Stm {
+  public Exp exp; 
+  public EXPR(Exp e) {exp=e;}
+  public ExpList kids() {return new ExpList(exp);}
+  public Stm build(ExpList kids) {
+    return new EXPR(kids.head);
+  }
+}
+
