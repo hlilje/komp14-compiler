55452d491c8e00b6082f0e20e3242c8da4f21bae
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-04-17 14:32:56 +0200

    Work on master test

--- test/execute/custom/MasterTest.java
+++ test/execute/custom/MasterTest.java
@@ -23,7 +23,48 @@ class Master {
 }
 
 class Useful {
+    int[] sortMe;
+
     public boolean start() {
+        sortMe = int[10];
+        sortMe[0] = 12345;
+        sortMe[1] = 0;
+        sortMe[2] = (123*234) - 444;
+        sortMe[3] = 44;
+        sortMe[4] = 23948;
+        sortMe[5] = 9245;
+        sortMe[6] = 1147;
+        sortMe[7] = 9348;
+        sortMe[8] = 123998;
+        sortMe[9] = 8;
+
+        this.sortArray(sortMe);
+    }
+
+    public boolean sortArray(int[] sortMe) {
+        int i;
+        int prev;
+        boolean cont;
+        i = 0;
+        cont = false;
+
+        while(i < sortMe.length || cont) {
+            int temp;
+            temp = sortMe[i];
+
+            if(sortMe[i] < temp) {
+
+            } else {
+            }
+            
+            i = i + i;
+
+            prev = sortMe[i];
+        }
+
         return true;
+    }
+
+    public boolean numberOfPrimes(int[] array) {
     }
 }
