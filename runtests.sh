4b8e2de96d8aceafe8d55320d178c01a4ea10073
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-04-25 12:05:16 +0200

    Add script to run all tests in a given folder

---
+++ runtests.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+# This script runs all tests in the given folder.
+# It expects 'mjc.jar' in the current directory.
+
+DIR="$1"
+
+if [ ! -d "$DIR" ]
+then
+    echo "Please enter a valid directory to run the tests"
+    exit 1
+fi
+
+if [ ! -f "mjc.jar" ]
+then
+    echo "Compiler jar 'mjc.jar' is missing"
+    exit 1
+fi
+
+for f in $DIR*.java
+do
+    echo "<<<< Now running test: `basename $f` >>>>"
+    java -jar mjc.jar $f
+    echo ""
+done
+
+exit 0
